00006b8cd53a274cd06932c2a24f84f4e8641fa8
timotheemcdonald <timotheemcdonald@gmail.com>
2020-07-17 11:48:37 -0400

    adding character name list

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character.js'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,38 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const baseURL = 'https://swapi.dev/api/people/';
+  const [character, setCharacter] = useState([])
+
+  console.log(baseURL);
+
+  useEffect(() => {
+    axios.get(baseURL)
+    .then(value => {
+      console.log('value start')
+      const newCharacter = value.data.results;
+      setCharacter(newCharacter);
+      console.log(newCharacter);
+      console.log('character success')
+
+    })
+    .catch(error => {
+      console.log('error')
+    })
+
+  }, [])
+
+  console.log(character);
+  
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {character.map(data => 
+         <Character name={data.name} id={data.id} propsPassed={data} />
+      )}
+     
+      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const Character = props => {
+
+    return (
+        <div>{props.name}</div>
+    )
+}
+
+export default Character;
